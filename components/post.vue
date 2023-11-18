83520c5a1be68f17b1290293c24b2bc0423e3853
Yegor Knyazev <egorknyazev10@gmail.com>
2023-11-18 19:04:49 +0400

    added some elements in post

--- components/post.vue
+++ components/post.vue
@@ -4,8 +4,12 @@
   <div class="container">
     <div style="background: url(https://www.belnovosti.by/sites/default/files/2023-04/kartoshka_2_0_0.jpg) 50% 50% no-repeat; background-size: cover; aspect-ratio: 1; width: 100%;" />
   
-    <div class="pa-4">
+    <div class="pa-4 pt-3">
+      <div class="font-weight-bold">Картошка</div>
+
+      <div style="line-height: 1.1; font-size: 15px;">Короткое описание моей картошечки</div>
       
+      <div style="font-size: 15px;" class="mt-2 font-weight-bold">50 руб/кг</div>
     </div>
   </div>
 </template>
