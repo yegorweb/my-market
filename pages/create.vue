de3107c38699d3a67e7651663597f3be6ab1d5d0
Yegor Knyazev <egorknyazev10@gmail.com>
2023-12-15 22:41:27 +0400

    added creating

---
+++ pages/create.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+useHead({
+  title: 'Опубликовать продукт'
+})
+</script>
+
+<template>
+  <v-container class="mt-header">
+    <v-btn to="/">
+back
+    </v-btn>
+    Создать
+  </v-container>
+</template>
